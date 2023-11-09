f7c4909a61a86111c0aff2d5cb9ddc28c1f13a2f
23wh1a0519 <150130821+23wh1a0519@users.noreply.github.com>
2023-11-09 21:04:58 +0530

    Update Hacker rank 1.c

--- 23WH1A0519/Hacker rank 1.c
+++ 23WH1A0519/Hacker rank 1.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+   char s1[50]="Welcome to C programming.";
+   printf("Hello, World!\n");
+   printf("%s",s1);
+}
 
