37f533d47de5a3f9302773b14e208714d5e02790
23wh1a0519 <150130821+23wh1a0519@users.noreply.github.com>
2023-11-09 21:10:45 +0530

    Update pattern HR3.c

--- 23WH1A0519/pattern HR3.c
+++ 23WH1A0519/pattern HR3.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
 
+    int n=0,i,j,len,start,end;
+    scanf("%d", &n);
+  len=2*n-1;
+  start=0;
+  end=len-1;
+  int a[len][len];
+while(n!=0)
+{
+    for(i=start;i<=end;++i)
+    {
+        for(j=start;j<=end;++j)
+        {
+            if(i==start||i==end||j==start||j==end)
+            a[i][j]=n;
+        }
+    }
+    ++start;
+    --end;
+    --n;
+}
+for(i=0;i<len;i++)
+{
+    for(j=0;j<len;j++)
+    {
+        printf("%d\t",a[i][j]);
+    }
+    printf("\n");
+}
+}
