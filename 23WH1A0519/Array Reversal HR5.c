932da123d1c6d82c4b55158f18e784c1d9e953fb
23wh1a0519 <150130821+23wh1a0519@users.noreply.github.com>
2023-11-09 21:17:10 +0530

    Update Array Reversal HR5.c

--- 23WH1A0519/Array Reversal HR5.c
+++ 23WH1A0519/Array Reversal HR5.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int num, arr[1000], i;
+    scanf("%d", &num);
+for(i = 1; i <=num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    
+
+    for(i = num; i>0; i--)
+        printf("%d ", *(arr + i));
+    return 0;
+}
