03952dabe21ac097bf9a9aa65bf4de555514c9c3
23wh1a0519 <150130821+23wh1a0519@users.noreply.github.com>
2023-11-09 21:14:59 +0530

    Update sum of integers in array HR4.c

--- 23WH1A0519/sum of integers in array HR4.c
+++ 23WH1A0519/sum of integers in array HR4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+ {
+   int n,sum=0,i;
+   scanf("%d",&n);
+   int arr[n];
+   for(i=0;i<n;i++)
+   {
+   scanf("%d",&arr[i]);
+   sum=sum+arr[i];
+   }
+  printf("%d",sum);
+}
 
