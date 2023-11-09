27f89d316243cc6760d73d4cc5536eb699207346
23wh1a0523 <150155882+23wh1a0523@users.noreply.github.com>
2023-11-09 21:18:15 +0530

    Add files via upload

---
+++ 23wh1a523.cpp
@@ -0,0 +1,13 @@
+// sum of multiples of 3 and 5
+#include<stdio.h>
+int main(){
+int i,sum=0;
+  	   for(i=1;i<1000;i++){
+
+  	       if(i%3==0 || i%5==0)
+  	          sum += i;
+  	    }
+  	printf("sum = %d",sum);
+  return 0;	
+  
+}
