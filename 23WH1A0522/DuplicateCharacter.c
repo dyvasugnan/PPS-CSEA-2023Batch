c24dc4104732005c91f66fd5a83cf20058e1a895
SIRISHA <150130671+23WH1A0522@users.noreply.github.com>
2023-11-12 09:51:16 +0530

    to find duplicate characters in string

---
+++ 23WH1A0522/DuplicateCharacter.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+  char word[50],dup[50];
+  int count=0;
+  printf("enter a word: ");
+  scanf("%s",word);
+  printf("the given word is: %s\n",word);
+  for(int i=0;i<strlen(word);i++){
+    for(int j=i+1;j<strlen(word);j++){
+        if(strchr(dup,word[i])!=0)
+        continue;
+      if(word[i]==word[j]){
+        dup[count]=word[i];
+        count++;
+      }
+    }
+  }
+  printf("%s",dup);
+  return 0;
+}
