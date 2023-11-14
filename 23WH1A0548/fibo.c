5186703ffe77f7f47d5642053238cd7e53eff560
23WH1A0548 <150130579+23WH1A0548@users.noreply.github.com>
2023-11-14 16:14:15 +0530

    Add files via upload

---
+++ 23WH1A0548/fibo.c
@@ -0,0 +1,14 @@
+//this is my program for fibo numbers
+#include<stdio.h>
+int main(){
+int num,value,t1=0,t2=1,t3,i;
+printf("enter the number: ");
+scanf("%d",&num);
+for(int i=1;i<=value;i++){
+ printf("%d",t1);
+ t3=t1+t2;
+ t1=t2;
+ t2=t3;
+}
+return 0;
+}
