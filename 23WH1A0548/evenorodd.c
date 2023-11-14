51858fcf425b9af2f2917b90af37c6addf26053d
23WH1A0548 <150130579+23WH1A0548@users.noreply.github.com>
2023-11-14 16:16:05 +0530

    Add files via upload

---
+++ 23WH1A0548/evenorodd.c
@@ -0,0 +1,14 @@
+//this is my even or odd using conditional statement
+#include<stdio.h>
+int main(){
+int n1;
+printf("enter 1 numbers: ");
+scanf("%d",&n1);
+if (n1%2==0)
+printf("%d is an even number",n1);
+else
+{
+printf("%d is an odd number",n1);
+}
+return 0;
+}
