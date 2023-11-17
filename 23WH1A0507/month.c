bf018c0700f6568946913067b3fb982a14742699
23WH1A0507 <150131831+23WH1A0507@users.noreply.github.com>
2023-11-17 21:07:07 +0530

    Add files via upload

---
+++ 23WH1A0507/month.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main(){
+	
+	int month_no;
+	printf("enter month no");
+	scanf("%d",&month_no);
+	switch(month_no){
+		case 1:
+			printf("31");
+			break;
+		case 2:
+			printf("28");
+			break;
+		case 3:
+			printf("31");
+			break;
+		case 4:
+			printf("30");
+			break;
+		case 5:
+			printf("31");
+			break;
+		case 6:
+			printf("30");
+			break;
+			
+		case 7:
+			printf("31");
+			break;	
+		case 8:
+			printf("31");
+			break;
+		case 9:
+			printf("30");
+			break;
+		case 10:
+			printf("31");
+			break;
+		case 11:
+			printf("30");
+			break;
+		case 12:
+			printf("31");
+			break;
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+}
