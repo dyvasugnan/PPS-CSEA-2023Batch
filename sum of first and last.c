8711bc30277ea5f47f2d49ec6a2791dd06b3587d
23WH1A0507 <150131831+23WH1A0507@users.noreply.github.com>
2023-11-17 21:07:38 +0530

    Add files via upload

---
+++ sum of first and last.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int num;
+	printf("enter number");
+	scanf("%d",&num);
+int	ld=num%10;
+num=num/10;int i,rem;
+	for(i=0;num!=0;i++){
+		rem=num%10;
+		num=num/10;
+		
+	}
+int	fd=rem;
+	printf("sum is %d",fd+ld);
+}
