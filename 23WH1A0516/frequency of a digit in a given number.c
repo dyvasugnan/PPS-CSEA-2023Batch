c2f542db5469d9e9a81a516e1c5b4d95b6371b8a
23wh1a0516 <150162158+23wh1a0516@users.noreply.github.com>
2023-11-17 23:21:42 +0530

    Add files via upload

---
+++ 23WH1A0516/frequency of a digit in a given number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int num,i;
+	printf("enter number :");
+	scanf("%d",&num);
+	int temp=num,count,rem;
+	for(i=0;i<=9;i++){
+		count=0;
+		while(num>0){
+			rem=num%10;
+			if(rem==i){
+				count++;
+			}
+			num=num/10;
+		}
+		printf("frequency of the %d = %d\n",i,count);
+		num=temp;
+	}
+	return 0;
+}
