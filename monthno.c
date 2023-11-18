873645f88505545639124910b220bb6ad2a04ea5
23WH1A0508 <150207921+23WH1A0508@users.noreply.github.com>
2023-11-18 07:22:02 +0530

    Add files via upload

---
+++ monthno.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main(){
+int num;
+printf("enter any number");
+scanf("%d",&num);
+switch(num){
+ case 1:
+  printf("31");
+  break;
+ case 2:
+  printf("28");
+  break;
+ case 3:
+  printf("31");
+  break;
+ case 4:
+  printf("30");
+  break;
+ case 5:
+  printf("31");
+  break;
+ case 6:
+  printf("30");
+  break;
+ case 7:
+  printf("31");
+  break;
+ case 8:
+  printf("30");
+  break;
+ case 9:
+  printf("31");
+  break;
+ case 10:
+  printf("30");
+  break;
+ case 11:
+  printf("31");
+  break;
+ case 12:
+  printf("30");
+  break;
+}    
+return 0;
+}
