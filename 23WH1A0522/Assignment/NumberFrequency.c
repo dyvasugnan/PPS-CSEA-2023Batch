dad8b2585191af7ecd2468484758a784e7919c41
SIRISHA <150130671+23WH1A0522@users.noreply.github.com>
2023-11-19 22:33:06 +0530

    frequency of a number in an array

---
+++ 23WH1A0522/Assignment/NumberFrequency.c
@@ -0,0 +1,26 @@
+//number frequency
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int num[10];
+    printf("enter any 10 numbers: ");
+    for(int i=0;i<10;i++)
+        scanf("%d",&num[i]);
+    for(int i=0;i<10;i++){
+        int count=0;
+        bool found=false;
+        for(int k=0;k<i;k++){
+            if(num[k]==num[i]){
+                found=true;
+                break;
+            }
+        }
+        if(found)
+            continue;
+        for(int j=0;j<10;j++){
+            if(num[i]==num[j])
+                count++;
+        }
+        printf("\nFrequency of %d: %d", num[i], count);
+    }
+}
