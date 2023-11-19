b12e694f43432cfeb418474f1539fde3fc041340
23wh1a0532 <150167075+23wh1a0532@users.noreply.github.com>
2023-11-19 13:29:24 +0530

    Add files via upload

---
+++ 23WH1A0532/swapping_num.c
@@ -0,0 +1,17 @@
+// this program is about swapping of first and last digits
+#include<stdio.h>
+int main(){
+ int a[10],n,i,temp;
+ printf("enter range of array");
+ scanf("%d",&n);
+ printf("enter elements of array:\n");
+ scanf("%d",&a[n]);
+ for(i=1;i<n;i++)
+ scanf("%d",&a[i]);
+ temp=a[0];
+ a[0]=a[n-1];
+ a[n-1]=temp;
+ for(i=0;i<n;i++)
+ printf("%d",a[i]);
+ return 0;
+}
