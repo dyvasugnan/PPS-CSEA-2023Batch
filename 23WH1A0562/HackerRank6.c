3915924e9d058e062d626439c817d08c140f96d3
23wh1a0562 <150153569+23wh1a0562@users.noreply.github.com>
2023-11-19 15:22:26 +0530

    Add files via upload

---
+++ 23WH1A0562/HackerRank6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,sum=0;
+    scanf("%d\n",&n);
+    int a[5000];
+    for(i=0;i<n;i++)
+    {
+      scanf("%d ",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+}
