04e486961660411ed58772e15f9ae6d2ade5fd8c
23WH1A0511 <150906509+23WH1A0511@users.noreply.github.com>
2023-11-19 16:40:58 +0530

    Create Frequencyofdigits.c

---
+++ Frequencyofdigits.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int num, i, count, dig, temp;
+printf("enter any number: ");
+scanf("%d", &num);
+temp=num;
+for(i=0;i<=9;i++) 
+{
+    count=0;
+    while(num! =0) {
+      dig=num%10;
+      if(i==dig) 
+           count++;
+     num=num/10;
+}
+printf(" frequency of %d=%d\n", i, count) ;
+num=temp;
+}
+return 0;
+}
