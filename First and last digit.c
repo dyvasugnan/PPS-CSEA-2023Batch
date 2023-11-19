3caad37c1e431d50852d0c7ac19b8a800faa5a2b
23WH1A0505 <150135423+23WH1A0505@users.noreply.github.com>
2023-11-19 15:28:16 +0530

    Create First and last digit.c 23wh1a0505

---
+++ First and last digit.c
@@ -0,0 +1,12 @@
+//23wh1a0505
+#include<stdio.h>
+int main(){
+int num:
+printf("enter any number: ");
+scanf("%d",&num);
+int last=num%10;
+while(num/10)!=0)
+     num=num/10;
+printf("%d",num);
+printf("sumof the first and last digit:%d",last+num);
+}
