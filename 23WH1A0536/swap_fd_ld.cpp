37f733fd5a785eceaf0b3cb61f6f31fd3087e2a7
23WH1A0536 <150166074+23WH1A0536@users.noreply.github.com>
2023-11-19 15:33:19 +0530

    Add files via upload

---
+++ 23WH1A0536/swap_fd_ld.cpp
@@ -0,0 +1,20 @@
+//To swap the first and last digits
+#include<stdio.h>
+#include<math.h>
+int main(){
+        int num, dig1, diglast,count = 0;
+        printf("Enter a number: ");
+        scanf("%d", &num);
+        diglast = num % 10;
+	int n = num, num1 = num;
+        for(int i = 0; num != 0; i++){
+                dig1 = num % 10;
+		count++;
+                num /= 10;
+        }
+	n = n - diglast + dig1;
+	n = n - (dig1-diglast) * pow(10,count-1);
+	printf("%d",n);
+	
+	return 0;
+}
