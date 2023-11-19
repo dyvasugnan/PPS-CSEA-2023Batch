add1cf14f550b4aa9e454a5a1aedb8ff6f850641
23WH1A0536 <150166074+23WH1A0536@users.noreply.github.com>
2023-11-19 15:27:17 +0530

    Add files via upload

---
+++ 23WH1A0536/sum_diff.c
@@ -0,0 +1,11 @@
+//To find the sum and difference of numbers using int and float datatypes
+#include <stdio.h>
+int main(){
+int n1,n2;
+float n3,n4;
+scanf("%d %d %f %f",&n1,&n2,&n3,&n4);
+printf("%d %d\n",n1+n2,n1-n2);
+printf("%0.1f %0.1f\n",n3+n4,n3-n4);
+    return 0;
+}
+
