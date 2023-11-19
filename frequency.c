78c062235cf473e428a1fa9fee93ca2257b9e260
saadia taqveem <150131526+23wh1a0525saadia@users.noreply.github.com>
2023-11-19 21:41:34 +0530

    Add files via upload

--- frequency.c
+++ frequency.c
@@ -1,27 +1,24 @@
-#include <stdio.h>
-#define BASE 10 /* Constant */
-int main()
-{
-    long long num, n;
-    int i, lastDigit;
-    int freq[BASE];
-    printf("Enter any number: ");
-    scanf("%d", &num);
-    for(i=0; i<BASE; i++)
-    {
-        freq[i] = 0;
-    }
-    n = num; 
-    while(n != 0)
-    {
-        lastDigit = n % 10;
-        n /= 10;
-        freq[lastDigit]++;
-    }
-    printf("Frequency of each digit in %l1d is: \n", num);
-    for(i=0; i<BASE; i++)
-    {
-        printf("Frequency of %d = %d\n", i, freq[i]);
-    }
-    return 0;
+#include<stdio.h>
+int main(){
+	int num;
+	int count[10]={0};
+	printf("enter a no");
+	scanf("%d",&num);
+	int i,rem,j;
+for(i=0;num!=0;i++){
+	rem=num%10;
+	num/=10;
+	for(j=0;j<=9;j++){
+	
+	
+	if(rem==j){
+		count[j]++;
+		
+	}
+}
+
+}
+for(i=0;i<=9;i++){
+	printf("frequency of %d = %d\n",i,count[i]);
+}
 }
