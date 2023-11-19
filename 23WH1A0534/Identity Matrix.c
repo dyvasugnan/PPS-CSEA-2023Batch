fce4b6c0229db5dbcd00e47b2039d017dacd5264
23WH1A0534 <150130026+23WH1A0534@users.noreply.github.com>
2023-11-19 17:55:45 +0530

    Add files via upload

---
+++ 23WH1A0534/Identity Matrix.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+    #include <stdlib.h>
+ 
+    void main() 
+    {
+ 
+        int a[100][100];
+        int i, j, r,c,sum=0,flag=1;
+ 
+        printf("Enter the order of the matrix\n");
+        scanf("%d %d", &r, &c);
+ 
+        printf("Enter the elements of matrix \n");
+        for (i = 0; i < r; i++)
+        {
+            for (j = 0; j < c; j++) 
+            {
+                scanf("%d", &a[i][j]);
+            }
+        }
+        printf("MATRIX : \n");
+        for (i = 0; i < r; i++)
+        {
+            for (j = 0; j < c; j++) 
+            {
+                printf("%3d", a[i][j]);
+            }
+            printf("\n");
+ 
+        }
+            for (i = 0; i < r; i++) 
+            {
+                for (j = 0; j < c; j++)
+                {
+                    if(i>j)
+                    {
+                        if (a[i][j]!=0)
+                        {
+                           flag=0;
+                           break;
+                        }
+                 }  }
+            }
+            for (i = 0; i < r; i++) 
+            {
+                for (j = 0; j < c; j++)
+                {
+                    if(j>i)
+                    {
+                        if (a[i][j]!=0)
+                        {
+                           flag=0;
+                           break;
+                        }
+                 }  }
+            }
+    
+    if(flag==1)
+    {
+        printf("Identity Matrix");
+    }
+    else
+    {
+      printf(" Not a Identity Matrix");
+    }
+}
+
+
+    
+
+
+
