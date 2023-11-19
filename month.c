208b73d6b422f0a650a74d8ed91bf35b04223fb9
AKHILA23WH1A0563 <150349875+AKHILA23WH1A0563@users.noreply.github.com>
2023-11-19 17:51:58 +0530

    Add files via upload

---
+++ month.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main() {
+ int month;
+printf("Enter month number (1-12): ");
+scanf("%d", &month);
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            printf("Number of days: 31\n");
+            break;
+        case 4: case 6: case 9: case 11:
+            printf("Number of days: 30\n");
+            break;
+        case 2:
+            printf("Number of days: 28 or 29 (leap year)\n");
+            break;
+        default:
+            printf("Invalid month number\n");
+    }
+
+    return 0;
+}
+
+
+    
+
+    
+
