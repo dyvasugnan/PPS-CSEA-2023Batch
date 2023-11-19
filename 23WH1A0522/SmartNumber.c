a92f5fe6a92a1ab2a7d288622d6eae6dabb2a151
SIRISHA <150130671+23WH1A0522@users.noreply.github.com>
2023-11-19 20:28:05 +0530

    summation of divisors below number is greater than number(print yes) or else(print no)

---
+++ 23WH1A0522/SmartNumber.c
@@ -0,0 +1,13 @@
+int main() {
+    int num,sum=0;
+    scanf("%d",&num);
+    for(int i=1;i<num;i++){
+    if(num%i==0)
+        sum = sum + i;
+    }
+    if(sum>num)
+        printf("Yes");
+    else
+        printf("No");
+    return 0;
+}
