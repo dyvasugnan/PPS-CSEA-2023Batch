4792d0fc29587235ce725f3b1890b271e5cd57b5
23WH1A0536 <150166074+23WH1A0536@users.noreply.github.com>
2023-11-19 15:17:01 +0530

    Add files via upload

---
+++ 23WH1A0536/power_num.c
@@ -0,0 +1,14 @@
+//to find the power of number using for loop
+#include<stdio.h>
+int main(){
+    int base,i,exp,pow=1;
+    printf("Base = ");
+    scanf("%d",&base);
+    printf("Exponent = ");
+    scanf("%d", &exp);
+    for(i=0; i<=exp; i++){
+             pow= pow*base;
+    }
+    printf("%d^%d=%d", base,exp,pow);
+    return 0;
+}    
