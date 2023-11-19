d299855f3405160eb8598ab92a94952b90df0921
23wh1a0513 <150132045+23wh1a0513@users.noreply.github.com>
2023-11-19 20:01:30 +0530

    Add files via upload

---
+++ smartnumber.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+ 
+    int n,sum = 0,i;
+    scanf("%d",&n);
+    for(i = 1 ;i < n ;i++)
+    {
+        if(n%i == 0)
+        {
+            sum  = sum + i;
+        }
+    }
+    if(sum > n)
+        printf("Yes");
+    else
+        printf("No");
+    
+    return 0;
+}
