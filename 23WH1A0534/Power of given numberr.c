13c2218a01be3f667a43f289d669dc45431ce249
23WH1A0534 <150130026+23WH1A0534@users.noreply.github.com>
2023-11-19 16:34:15 +0530

    Add files via upload

---
+++ 23WH1A0534/Power of given numberr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int p=1;
+    int b,e,i;
+    printf("Enter base");
+    scanf("%d",&b);
+    printf("Enter exponent");
+    scanf("%d",&e);
+    for(i=1;i<=e;i++)
+    {
+      p=p*b;  
+    }
+    printf("%d ^ %d = %d",b,e,p);
+}
