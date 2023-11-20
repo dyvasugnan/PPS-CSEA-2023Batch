0d41633530f73700a15731d1cabfc455ab5d57ac
23WH1A0505 <150135423+23WH1A0505@users.noreply.github.com>
2023-11-20 08:24:57 +0530

    Create Swapping number.c 23wh1a0505

---
+++ Swapping number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+  int n,on,f,c=0,power=1;
+printf("enter the number: ");
+scanf("%d",&n);
+on=n
+  int 1=n%10;
+  while((n/10)!=0){
+       n=n/10;
+       c++;
+}
+f=n;
+n=on;
+for(int i=0;i<c;i++){
+power=power*10;
+}
+n=power%10;
+n=n/10;
+printf("number after swaping first a last:%d",first+last,n,f,c,p);
+}
+
+    
+  
