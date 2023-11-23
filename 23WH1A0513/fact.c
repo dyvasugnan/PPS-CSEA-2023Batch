b1f862e664a122a0b8fe32ec14203b4dff2ecf59
23wh1a0513 <150132045+23wh1a0513@users.noreply.github.com>
2023-11-23 19:03:52 +0530

    Add files via upload

---
+++ 23WH1A0513/fact.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num,fact, i;
+    printf("enter any number");
+    scanf("%d",&num);
+    for(i=0;1<=num;i++)
+    {
+        fact=fact*i;
+    }
+    printf(fact="%d",fact);
+    return 0;
+}
+
