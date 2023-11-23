c82bf3eef8221df8d86bc73e34b114f685b61949
23WH1A0508 <150207921+23WH1A0508@users.noreply.github.com>
2023-11-23 19:26:21 +0530

    Add files via upload

---
+++ 23wh1a0508/largestprime.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c=0,max=0;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+        c=0;
+        if(n%i==0)
+        {
+            for(j=1;j<=i;j++) 
+            {
+                if(i%j==0)
+                {
+                    c++;
+                }
+            }
+            if(c==2)
+            {
+                if(i>max)
+                max=i;
+            }
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
+
+
