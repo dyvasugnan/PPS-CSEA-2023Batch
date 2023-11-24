c2303c0f684739775953882f3c864fab307eb12a
23wh1a0535-sadhana <150142098+23wh1a0535-sadhana@users.noreply.github.com>
2023-11-24 20:28:02 +0530

    Update euler1.c

--- 23WH1A0535/euler1.c
+++ 23WH1A0535/euler1.c
@@ -1,14 +1,31 @@
+#include <math.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+
 int main() 
 {  
-  int i,sum=0;
-  for(i=1;i<1000;i++)
+  int i, sum = 0, sum1 = 0;
+  for(i=1;i<10;i++)
   {
-      if(i%3==0||i%5==0)
+      if(i%3==0 || i%5==0)
       {
-         sum=sum+i;
+         sum = sum+i;
       }
   }
-  printf("%d",sum);
+  printf("%d\n", sum);
+      for(i=1;i<100;i++)
+  {
+      if(i%3==0 || i%5==0)
+      {
+         sum1 = sum1+i;
+      }
+  }
+  printf("%d", sum1);
+    
     return 0;
 }
