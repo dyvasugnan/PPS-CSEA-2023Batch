0a5ecd3fce2e86b089823f0d3c5493374b6c243f
23wh1a0535-sadhana <150142098+23wh1a0535-sadhana@users.noreply.github.com>
2023-11-24 18:30:57 +0530

    Create functions hr6.c

---
+++ 23WH1A0535/functions hr6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void max_of_four(int ,int, int, int);
+int main()
+{ 
+    int a,b,c,d;
+    scanf("%d%d%d%d", &a ,&b, &c, &d);
+    max_of_four(a,b,c,d);
+    
+}
+
+void max_of_four(int a,int b,int c,int d)
+{
+    if(a>b && a>c &&  a>d) 
+        printf("%d", a); 
+    else if(b>c && b>d) 
+        printf("%d", b); 
+    else if(c>d) 
+        printf("%d", c);
+    else
+        printf("%d",d);   
+}
