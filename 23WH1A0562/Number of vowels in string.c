fe7a1d8448730c6c5143c8e8b2d8bce27ffefcc5
23wh1a0562 <150153569+23wh1a0562@users.noreply.github.com>
2023-11-27 20:51:59 +0530

    Add files via upload

---
+++ 23WH1A0562/Number of vowels in string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{    void isVowel();
+   {
+    char arr[50]="aeiouAeiou";
+    int count=0,i;
+    for(i=0;i<50;i++)
+    {
+        if(arr[i]=='a'||arr[i]=='e'||arr[i]=='i'||arr[i]=='o'||arr[i]=='u')
+        count++;
+    }
+    printf("%d",count);
+    
+   }
+   void main()
+   {
+       isVowel();
+       
+   }
+}
