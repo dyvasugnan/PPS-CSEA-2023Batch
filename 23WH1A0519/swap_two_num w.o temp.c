549c2ff9b78a91a9a47e28c9300ff7ead10c5c73
23wh1a0519 <150130821+23wh1a0519@users.noreply.github.com>
2023-12-03 20:08:42 +0530

    Create swap_two_num w.o temp.c

---
+++ 23WH1A0519/swap_two_num w.o temp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("Enter first number:");
+    scanf("%d",&num1);
+    printf("Enter second number:");
+    scanf("%d",&num2);
+       num1=num1*num2;
+       num2=num1/num2;
+       num1=num1/num2;
+    printf("%d,%d",num1,num2);
+}
