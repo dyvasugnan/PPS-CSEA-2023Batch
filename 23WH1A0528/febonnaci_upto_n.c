e34efaaf29e01406b5e67e6b6f05da82cf03ac4d
23wh1a0528 <150130964+23wh1a0528@users.noreply.github.com>
2023-12-04 22:50:55 +0530

    Update febonnaci_upto_n.c

---
+++ 23WH1A0528/febonnaci_upto_n.c
@@ -0,0 +1,15 @@
+//to display fibonacci sequence upto n terms
+#include<stdio.h>
+int main() {
+int num,t0=0,t1=1,next=0;
+printf("enter a value");
+scanf("%d",&num);
+printf("%4d%4d",t0,t1);
+for(int i=1; i<=num; i++){
+printf("%4d",next);
+t0=t1;
+t1=next;
+next = t0+t1;
+}
+return 0;
+}
