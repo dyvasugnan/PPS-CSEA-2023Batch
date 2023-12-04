95c1670bb6f4acdc41d7d956ef791b570ddd79ba
23wh1a0528 <150130964+23wh1a0528@users.noreply.github.com>
2023-12-04 23:37:52 +0530

    Update and rename gcd.c to gcd_function.c

--- 23WH1A0528/gcd_function.c
+++ 23WH1A0528/gcd_function.c
@@ -5,4 +5,12 @@ int main(){
 	int num1,num2;
 	printf("enter the numbers: ");
 	scanf("%d%d",&num1,&num2);
-	int result=gcd
+	gcd(num1,num2);
+return 0;
+}
+int gcd(int num1, int num2){
+	for(int i=1; i<=num1 && i<=num2; i++){
+		if(num1%i==0 && num2%i==0)
+			printf("d",i);
+	}
+}
