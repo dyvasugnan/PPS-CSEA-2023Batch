c8a9d9fe2f449969de98cac36b8bd2c1515bb7da
23WH1A0511 <150906509+23WH1A0511@users.noreply.github.com>
2023-12-30 16:02:28 +0530

    Create Student grade.c

---
+++ 23WH1A0511/Student grade.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int sub1,sub2,sub3,sub4,sub5,sub6;
+printf("enter the marks of six subjects: ");
+scanf("%d%d%d%d%d%d",&sub1,&sub2,&sub3,&sub4,&sub5,&sub6);
+int sum=(sub1+sub2+sub3+sub4+sub5+sub6);
+int avg=sum/6;
+if(avg>=90)
+printf("A+");
+else if(avg>=80 && avg<90)
+printf("A");
+else if(avg>=70 && avg<80)
+printf("B+");
+else if(avg>=60 && avg<70)
+printf("B");
+else if(avg>=50 && avg<50)
+printf("C");
+else
+printf("fail");
+return 0;
+}
