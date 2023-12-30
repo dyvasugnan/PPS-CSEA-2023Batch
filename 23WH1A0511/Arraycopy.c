f42a198722b4654ba369af70ed5af9f93c568869
23WH1A0511 <150906509+23WH1A0511@users.noreply.github.com>
2023-12-30 11:05:44 +0530

    Create Arraycopy.c

---
+++ 23WH1A0511/Arraycopy.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+int numbers[20],size,i;
+printf("enter the number of numbers to be entered: ");
+scanf("%d",&size);
+printf("enter the numbers: ");
+for(i=0;i<size;i++){
+	scanf("%d",&numbers[i]);
+}
+for(i=(size-1);i>=0;i--){
+	printf("%d",numbers[i]);
+}
+}
