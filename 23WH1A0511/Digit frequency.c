530cd292166fea67f97a048eb804ab8426ba8d3b
23WH1A0511 <150906509+23WH1A0511@users.noreply.github.com>
2023-12-30 15:56:20 +0530

    Create Digit frequency.c

---
+++ 23WH1A0511/Digit frequency.c
@@ -0,0 +1,19 @@
+//frequency of digits in a number
+#include<stdio.h>
+void main(){
+int num,i,count,dig,temp;
+printf("enter any number: ");
+scanf("%d",&num);
+temp=num;
+for(i=0;i<=9;i++){
+	count=0;
+	while(num!=0){
+		dig=num%10;
+		if(i==dig)
+			count++;
+		num=num/10;
+	}
+printf("frequency of %d= %d\n",i,count);
+num=temp;
+}
+}
