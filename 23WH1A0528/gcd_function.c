e7fd004a9c0b14ae86e3a2589702c6530ec1bd46
23wh1a0528 <150130964+23wh1a0528@users.noreply.github.com>
2023-12-31 17:47:50 +0530

    Update gcd_function.c

--- 23WH1A0528/gcd_function.c
+++ 23WH1A0528/gcd_function.c
@@ -5,12 +5,15 @@ int main(){
 	int num1,num2;
 	printf("enter the numbers: ");
 	scanf("%d%d",&num1,&num2);
-	gcd(num1,num2);
+int result = gcd(num1,num2);
+printf("%d",result);
 return 0;
 }
 int gcd(int num1, int num2){
+    int i,gcd;
 	for(int i=1; i<=num1 && i<=num2; i++){
 		if(num1%i==0 && num2%i==0)
-			printf("d",i);
+	        gcd = i;	    
 	}
+	return gcd;
 }
